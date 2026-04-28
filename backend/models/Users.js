9cdf852d2c4738eb04add992e2c9c7d7fe340a8e
nabilnir <nirarhan@gmail.com>
2026-04-28 23:13:10 +0600

    User schema added for Database

---
+++ backend/models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { unique } = require('next/dist/build/utils');
+
+const UserSchema = new mongoose.Schema({
+    fullName : { type: String, require : true},
+    email    : { type: String, require : true, unique : true},
+    password : { type: String, require : true},
+    role     : { type: String, erum:['teacher', 'student'], require : true},
+    institution : { type: String},
+}, { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
+
